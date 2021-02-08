35b4728fb26c2d413c9be543f54931e258eefbfc
ken <c.uvsuv@163.com>
2021-02-08 10:53:21 +0800

    publish 1.0.0

--- app/src/main/java/com/show/example/MainActivity.kt
+++ app/src/main/java/com/show/example/MainActivity.kt
@@ -2,10 +2,31 @@ package com.show.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.lifecycle.MutableLiveData
+import com.show.livedataktx.debounce
+import com.show.livedataktx.filter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data = MutableLiveData<String>()
+
+
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            data.value = "4500"
+        }
+
+
+
+        data.debounce(300)
+                .filter { it!=null }
+                .observe(this){
+                    Log.e("MainActivity", it)
+                }
+
     }
 }
